5ab0794b4fe9a89d8f7b522e1e3e7c2cb4742801
nicolamenace <nicolasmadec999@gmail.com>
2025-04-11 11:30:37 +0200

    Final

---
+++ _app/immutable/chunks/CXHjsjdU.js
@@ -0,0 +1,1 @@
+var s;const l=((s=globalThis.__sveltekit_nlladz)==null?void 0:s.base)??"/mdcnico.github.io";var a;const t=((a=globalThis.__sveltekit_nlladz)==null?void 0:a.assets)??l;export{t as a,l as b};
