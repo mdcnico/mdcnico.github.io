8cc7a9a3ed01c70121f97295cc8dceb570202a48
nicolamenace <nicolasmadec999@gmail.com>
2025-04-22 10:55:03 +0200

    Modification de la page de la 3e année

---
+++ _app/immutable/chunks/D6Xum9Mi.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_2j4u46)==null?void 0:s.base)??"/mdcnico.github.io";var t;const e=((t=globalThis.__sveltekit_2j4u46)==null?void 0:t.assets)??a;export{e as a,a as b};
